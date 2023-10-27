11f0a28f2b0f62b2d1bcfba8642383d46f460d95
kukkitarou <njb-c22004@sist.ac.jp>
2023-10-27 14:07:56 +0900

    ランダム

---
+++ test3 - コピー/test3/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace test3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.image1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.image1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // image1
+            // 
+            this.image1.Image = global::test3.Properties.Resources.suzuka;
+            this.image1.Location = new System.Drawing.Point(1098, 430);
+            this.image1.Name = "image1";
+            this.image1.Size = new System.Drawing.Size(110, 93);
+            this.image1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.image1.TabIndex = 0;
+            this.image1.TabStop = false;
+            this.image1.Click += new System.EventHandler(this.image1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(433, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "スタート";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(560, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 44);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "停止";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1898, 1024);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.image1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.image1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox image1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ test3 - コピー/test3/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test3
+{
+    public partial class Form1 : Form
+    {
+        //左右判定用、falseが右から左、tureが左から右
+        bool cheage = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //枠を消す
+            this.FormBorderStyle = FormBorderStyle.None;
+
+            //ファームを背景色で透明化する
+            this.TransparencyKey = BackColor;
+
+            //画面全体
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+
+            timer1.Interval = 100;
+            timer1.Enabled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //スタート
+            timer1.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //停止
+            timer1.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //if (cheage == false)
+            //{
+
+            //    image1.Left = image1.Left + -10;
+            //    //image1.Top = image1.Top + 10;
+            //}
+            //else
+            //{
+            //    image1.Left = image1.Left + 10;
+
+            //}
+
+            Random rnd = new Random();
+
+            int x = rnd.Next(0, 100);
+
+            if (x <= 24)//上
+            {
+                image1.Top = image1.Top + -10;
+                image1.Top = image1.Top + -10;
+                image1.Top = image1.Top + -10;
+                image1.Top = image1.Top + -10;
+                image1.Top = image1.Top + -10;
+            }
+            else if (x <= 49)//下
+            {
+
+                image1.Top = image1.Top + 10;
+                image1.Top = image1.Top + 10;
+                image1.Top = image1.Top + 10;
+                image1.Top = image1.Top + 10;
+                image1.Top = image1.Top + 10;
+            }
+            else if (x <= 74)//左
+            {
+                image1.Left = image1.Left + -10;
+                image1.Left = image1.Left + -10;
+                image1.Left = image1.Left + -10;
+                image1.Left = image1.Left + -10;
+                image1.Left = image1.Left + -10;
+            }
+            else if (x >= 99)//右
+            {
+                image1.Left = image1.Left + 10;
+                image1.Left = image1.Left + 10;
+                image1.Left = image1.Left + 10;
+                image1.Left = image1.Left + 10;
+                image1.Left = image1.Left + 10;
+            }
+        }
+
+        private void image1_Click(object sender, EventArgs e)
+        {
+            //反転
+            image1.Image.RotateFlip(RotateFlipType.RotateNoneFlipX);
+            image1.Refresh();
+
+
+            //左右切り替え
+            if (cheage == true)
+            {
+                cheage = false;
+            }
+            else
+            {
+                cheage = true;
+            }
+
+        }
+
+    }
+
+    
+}
+//google
+//string target = "https://www.google.co.jp/";
+//try
+//{
+//    System.Diagnostics.Process.Start(target);
+//}
+//catch (System.ComponentModel.Win32Exception noBrowser)
+//{
+//    if (noBrowser.ErrorCode == -2147467259)
+//        MessageBox.Show(noBrowser.Message);
+//}
+//catch (System.Exception other)
+//{
+//    MessageBox.Show(other.Message);
+//}
